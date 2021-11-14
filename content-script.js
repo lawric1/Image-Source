95b49e6fdf44a5e890d0bf102c6fb9922f3e973f
lawric1 <67882089+lawric1@users.noreply.github.com>
2021-11-14 11:00:20 -0300

    .

--- content-script.js
+++ content-script.js
@@ -1,18 +1,29 @@
 var searchButton = document.createElement('a');
 searchButton.style.cssText = `
     position: absolute;
+    padding: 6px;
 `;
 searchButton.target = "_blank"
 
 var buttomIcon = document.createElement('img');
 buttomIcon.src = chrome.runtime.getURL("images/search-icon.png");
-buttomIcon.style.width = "32px";
+buttomIcon.style.cssText = `
+    padding: 4px;
+    background-color: rgba(0, 0, 0);
+    border-radius: 8px;
+    width: 32px;
+`;
 searchButton.appendChild(buttomIcon);
 
 
+function findSource(image) {
+    var url = "https://www.google.com/searchbyimage?site=search&sa=X&image_url=" + image;
+}
 
-function createSearchButton(rectTop, rectLeft, imgSource) {
-    searchButton.href = imgSource;
+
+function createSearchButton(rectTop, rectLeft, image) {
+    // var imageSource = findSource(image);
+    searchButton.href = image;
     searchButton.style.left = rectLeft + 'px';
     searchButton.style.top = rectTop + window.scrollY + 'px';
     
@@ -23,11 +34,10 @@ function createSearchButton(rectTop, rectLeft, imgSource) {
 function addSearchButton(event){
     var target = event.target;
     if (target.tagName.toLowerCase() === 'img') {
-        var elementRect = target.getBoundingClientRect()
+        var elementRect = target.getBoundingClientRect();
 
-        createSearchButton(elementRect.top, elementRect.left, target.src)
+        createSearchButton(elementRect.top, elementRect.left, target.src);
     }
 }
 
 document.body.addEventListener('mouseover', addSearchButton, false);
-
